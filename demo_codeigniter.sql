b9434fbe108329fb960fd3e1054e3e9718b78682
Rajesh Mudaliar <web_dev_2@shubantecgnologies.com>
2018-03-23 17:37:57 +0530

    new sql
    new sql

--- demo_codeigniter.sql
+++ demo_codeigniter.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 10, 2018 at 02:03 PM
+-- Generation Time: Mar 23, 2018 at 01:06 PM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 5.6.31
 
@@ -44,9 +44,8 @@ CREATE TABLE `product` (
 INSERT INTO `product` (`prod_id`, `prod_name`, `prod_quantity`, `prod_price`, `prod_stock`, `prod_image`) VALUES
 (1, 'Test', '12', '12', '12', '15206858219L4m5ip.jpg'),
 (8, 'asas', '1212', '121212', '1212', '1520686525pro3.png'),
-(9, 'asasasas', '121212', '21121212', '121212', '1520686525pro2 .jpeg'),
-(11, 'asdas', 'dasdasd', 'asdasd', 'asdasdas', '1520686862favicon.png'),
-(16, 'asd', 'asdasd', 'asdas', 'dasdasd', '1520686951pro2 .jpeg');
+(16, 'asd', 'asdasd', 'asdas', 'dasdasd', '1520686951pro2 .jpeg'),
+(17, 'asas', '1212', '2121', '212', '1520923255pro2 .jpeg');
 
 -- --------------------------------------------------------
 
@@ -62,16 +61,17 @@ CREATE TABLE `users` (
   `u_password` varchar(200) NOT NULL,
   `u_phone` varchar(20) NOT NULL,
   `u_profile` text NOT NULL,
-  `u_about` text NOT NULL
+  `u_about` text NOT NULL,
+  `u_auth` enum('0','1','2') NOT NULL DEFAULT '0' COMMENT '0-Normal, 1-Gplus, 2-FB'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`u_id`, `u_firstname`, `u_lastname`, `u_email`, `u_password`, `u_phone`, `u_profile`, `u_about`) VALUES
-(1, 'Rajesh M', 'Mudaliar', 'rajeshmudaliar1126@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '9876543210', '1520675707icon-114.png', 'Test'),
-(3, 'Kalpesh', 'Patel', 'kalpesh@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '987654321', '1520675605pro3.png', 'asas');
+INSERT INTO `users` (`u_id`, `u_firstname`, `u_lastname`, `u_email`, `u_password`, `u_phone`, `u_profile`, `u_about`, `u_auth`) VALUES
+(1, 'Rajesh', 'Mudaliar', 'rajeshmudaliar1126@gmail.com', '', '9876543210', '1521790457_image.jpg', '', '1'),
+(2, 'Kalpesh', 'Patel', 'k4kalpesh125@gmail.com', '', '', '1521790514_image.jpg', '', '1');
 
 --
 -- Indexes for dumped tables
@@ -102,7 +102,7 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;COMMIT;
+  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
